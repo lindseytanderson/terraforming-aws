77b6b872cf5a6d4e2bb31dc6af6db0981f52ed9e
Lindsey Anderson <landerson@pivotal.io>
2019-11-12 09:04:57 -0500

    Addition of Classic Load Balancer for PKS Kubernetes Master node. 	- Security Group added to allow access from outside world

--- modules/pks/lb.tf
+++ modules/pks/lb.tf
@@ -32,6 +32,7 @@ resource "aws_elb" "pks_api" {
   name                             = "${var.env_name}-pks-api-clb"
   availability_zones               = ["${var.availability_zones}"]
   cross_zone_load_balancing        = true
+  security_groups                  = [${aws_security_group.pks_api_lb_security_group}]
 
   listener {
     instance_port = 8443
@@ -46,5 +47,44 @@ resource "aws_elb" "pks_api" {
     lb_protocol = "tcp"
   }
 
-  tags = "${merge(var.tags, map("Name", "${var.env_name}-pks-api-clb"))}"
+  tags = "${var.tags}"
+}
+
+// Allow access to the Kubernetes Master node
+resource "aws_security_group" "pks_k8s_master_lb_security_group" {
+  name        = "pks_k8smaster_lb_security_group"
+  description = "PKS K8s Master LB Security Group"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 8443
+    to_port     = 8443
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = "${merge(var.tags, map("Name", "${var.env_name}-pks-k8smaster-lb"))}"
+}
+
+resource "aws_elb" "pks_k8s_master" {
+  name                             = "${var.env_name}-pks-k8smaster-clb"
+  availability_zones               = ["${var.availability_zones}"]
+  cross_zone_load_balancing        = true
+  security_groups                  = [${aws_security_group.pks_k8s_master_lb_security_group}]
+
+  listener {
+    instance_port = 8443
+    instance_protocol = "tcp"
+    lb_port = 8443
+    lb_protocol = "tcp"
+  }
+
+  tags = "${var.tags}"
 }
